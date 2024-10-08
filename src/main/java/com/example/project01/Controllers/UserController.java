16083423e4c6e86b6e44996adf6cc7f68b4c408d
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2024-10-08 16:04:20 +0300

    feat: update UserController and UserService

--- src/main/java/com/example/project01/Controllers/UserController.java
+++ src/main/java/com/example/project01/Controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.example.project01.Controllers;
 
+import com.example.project01.Entities.Post;
+import com.example.project01.Entities.User;
 import com.example.project01.Services.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -12,4 +15,30 @@ public class UserController {
     public UserController(UserService userService){
         this.userService = userService;
     }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getOneUser(@PathVariable Long userId){
+        return userService.getOneUserById(userId);
+    }
+
+    @PostMapping
+    public User createOneUser(@RequestBody User newUser){
+        return userService.createOneUserById(newUser);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateOneUser(@PathVariable Long userId, @RequestBody User newUser){
+        return userService.updateOneUserById(userId, newUser);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteOneUser(@PathVariable Long userId){
+        userService.deleteOneUserById(userId);
+    }
+
 }
--- src/main/java/com/example/project01/Services/UserService.java
+++ src/main/java/com/example/project01/Services/UserService.java
@@ -1,7 +1,11 @@
 package com.example.project01.Services;
 
+import com.example.project01.Entities.User;
 import com.example.project01.Repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -9,5 +13,35 @@ public class UserService {
 
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getOneUserById(Long userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    public User createOneUserById(User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    public User updateOneUserById(Long userId, User newUser) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()){
+            User foundUser = new User();
+            foundUser = user.get();
+            foundUser.setUsername(newUser.getUsername());
+            userRepository.save(foundUser);
+            return foundUser;
+
+        } else{
+            return null;
+        }
+    }
+
+    public void deleteOneUserById(Long userId) {
+        userRepository.deleteById(userId);
     }
 }
